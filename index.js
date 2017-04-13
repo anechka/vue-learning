68fc8d3fe9843ce446b4c2ef4503496d52cdb09f
menangen <menangen@gmail.com>
2017-04-13 09:30:54 +0300

    Cats description

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-var model = [
-    {catName: "Pusya", syt: false},
-    {catName: "Mosha", syt: false},
-    {catName: "Oposya", syt: false},
-    {catName: "Kusya", syt: true},
-    {catName: "Persik", syt: false},
-    {catName: "Monja", syt: true},
-    {catName: "Musja", syt: true},
-    {catName: "Masya", syt: false}
-];
+var model = {
+    state: {
+        description: "Good Cat",
+        hasFoodNow: 0,
+
+        cats: [
+            {catName: "Pusya", syt: false, description: "Серая"},
+            {catName: "Mosha", syt: false, description: "Турок"},
+            {catName: "Oposya", syt: false, description: "Флегматичный"},
+            {catName: "Kusya", syt: true, description: "Пятнистый"},
+            {catName: "Persik", syt: false, description: "Песчаный"},
+            {catName: "Monja", syt: true, description: "Старая"},
+            {catName: "Musja", syt: true, description: "Черная"},
+            {catName: "Masya", syt: false, description: "Смешная"}
+        ]
+    }
+};
 
 Vue.component("cat", {
-    props: ["name"],
-    template: "<span>{{ name }}</span>"
+    props: ["name", "description"],
+    methods: {
+        clickHdlr: function (e) {
+            console.log("Clicked on: " + this.name, "has description: " + this.description);
+
+            model.state.description = this.description;
+        }
+    },
+    template: "<span @click='clickHdlr'>{{ name }}</span>"
 });
 
 Vue.component("icon", {
-    template: "<span class='glyphicon glyphicon-cutlery miska'></span>"
+    template: "<span class='glyphicon glyphicon-cutlery bowl'></span>"
 });
 
-Vue.component("miska", {
+Vue.component("bowl", {
     render: function (createElement) {
         var myComponent = {
-            template: "<span><icon></icon>Покушали</span>"
+            data: function() {
+                return {
+                    count: model.state.hasFoodNow
+                }
+            },
+            template: "<span><icon></icon>Покушали {{ count }}</span>"
         };
 
         return createElement(myComponent)
@@ -33,25 +52,28 @@ var app = new Vue({
     data: {
         hello: "Hello Vue!",
         name: "Аня!",
+        bowlValue: 2,
 
-        catList: model
+        sharedState: model.state
     },
     computed: {
         catsObject: function () {
             var shouldFeedCount = 0;
 
-            for (cat in this.catList) {
-                if (!this.catList[cat].syt) shouldFeedCount++
+            for (cat in this.sharedState.cats) {
+                if (!this.sharedState.cats[cat].syt) shouldFeedCount++
             }
 
-            return {count: shouldFeedCount, miski: Math.round(shouldFeedCount / 2)};
+            return { count: shouldFeedCount, bowls: Math.round(shouldFeedCount / this.bowlValue) };
         }
     },
     methods: {
         feedCat: function (e) {
-            for (cat in this.catList) {
-                this.catList[cat].syt = !this.catList[cat].syt;
+            for (cat in this.sharedState.cats) {
+                this.sharedState.cats[cat].syt = !this.sharedState.cats[cat].syt;
             }
+
+            model.state.hasFoodNow = model.state.cats.length - this.catsObject.count
         }
     }
 });
