01020b7cbe876ef6956412ed305c1483371c1f83
Andrey Menangen <menangen@gmail.com>
2017-05-11 19:26:16 +0300

    Model with "age" attribute

--- populateDB.py
+++ populateDB.py
@@ -7,26 +7,27 @@ import django
 def populate():
     from cats.models import Cat
 
-    def add_cat(name, fed, description):
-        c = Cat.objects.get_or_create(name=name, fed=fed, description=description)
+    def add_cat(name, age, fed, description):
+        c = Cat.objects.get_or_create(name=name, age=age, fed=fed, description=description)
         return c
 
     cats = [
-        {"catName": "Pusya", "syt": False, "description": u"Серая"},
-        {"catName": "Mosha", "syt": False, "description": u"Турок"},
-        {"catName": "Oposya", "syt": False, "description": u"Флегматичный"},
-        {"catName": "Kusya", "syt": True, "description": u"Пятнистый"},
-        {"catName": "Persik", "syt": False, "description": u"Песчаный"},
-        {"catName": "Monja", "syt": True, "description": u"Мудрая"},
-        {"catName": "Musja", "syt": True, "description": u"Длинноногая"},
-        {"catName": "Masya", "syt": False, "description": u"Смешная"}
+        {"catName": "Pusya", "age": 6, "syt": False, "description": u"Серая"},
+        {"catName": "Mosha", "age": 11, "syt": False, "description": u"Турок"},
+        {"catName": "Oposya", "age": 9, "syt": False, "description": u"Флегматичный"},
+        {"catName": "Kusya", "age": 5, "syt": True, "description": u"Пятнистый"},
+        {"catName": "Persik", "age": 1, "syt": False, "description": u"Песчаный"},
+        {"catName": "Monja", "age": 15, "syt": True, "description": u"Мудрая"},
+        {"catName": "Musja", "age": 8, "syt": True, "description": u"Длинноногая"},
+        {"catName": "Masya", "age": 1, "syt": False, "description": u"Смешная"}
     ]
 
     for cat in cats:
-        name, fed, description = cat["catName"], cat["syt"], cat["description"]
+        name, age, fed, description = cat["catName"], cat["age"], cat["syt"], cat["description"]
 
         add_cat(
             name=name,
+            age=age,
             fed=fed,
             description=description
             )
